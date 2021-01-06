b30c00481449984670126ad131ec4eeb478c536a
Aikusoid <74870164+Aikusoid@users.noreply.github.com>
2021-01-06 10:29:59 +0100

    Weather discription, current weather icon

--- src/script.js
+++ src/script.js
@@ -3,7 +3,7 @@ function formatDate(date) {
   let currentDate = date.getDate();
   let months = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
   let currentMonth = months[date.getMonth()];
-  if (currentMonth < 10){
+  if (currentMonth < 10) {
     currentMonth = `0${currentMonth}`;
   }
   let currentYear = date.getFullYear();
@@ -26,30 +26,67 @@ function formatTime(date) {
   return `${hours}:${minutes}:${seconds}`;
 }
 
-function displayWeather(response){
-  document.querySelector("#required-city").innerHTML = `${response.data.name}, ${response.data.sys.country}`;
+function displayWeather(response) {
+  document.querySelector(
+    "#required-city"
+  ).innerHTML = `${response.data.name}, ${response.data.sys.country}`;
 
-  document.querySelector("span.actual-temperature").innerHTML = Math.round(response.data.main.temp);
+  document.querySelector("span.actual-temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
 
-  document.querySelector("#fahrenheit").addEventListener("click", function (event){
-    event.preventDefault();
-    document.querySelector("span.actual-temperature").innerHTML = Math.round((response.data.main.temp)*1.8+32);
-  }); 
-  
-  document.querySelector("#celsius").addEventListener("click", function (event){
-    event.preventDefault();
-    document.querySelector("span.actual-temperature").innerHTML = Math.round(response.data.main.temp);
-  }); 
+  document.querySelector("#description").innerHTML =
+    response.data.weather[0].description;
+
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    response.data.main.feels_like
+  );
+
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+
+  document.querySelector("#wind-speed").innerHTML = Math.round(
+    response.data.wind.speed
+  );
+
+  document.querySelector("#wind-direction").innerHTML = response.data.wind.deg;
+
+  document.querySelector("#pressure").innerHTML = response.data.main.pressure;
+
+  document
+    .querySelector("#current-icon-description")
+    .setAttribute(
+      "src",
+      "http://openweathermap.org/img/w/" +
+        response.data.weather[0].icon +
+        ".png"
+    );
+  document
+    .querySelector("#fahrenheit")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      document.querySelector("span.actual-temperature").innerHTML = Math.round(
+        response.data.main.temp * 1.8 + 32
+      );
+    });
+
+  document
+    .querySelector("#celsius")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      document.querySelector("span.actual-temperature").innerHTML = Math.round(
+        response.data.main.temp
+      );
+    });
 }
 
-function currentLocation(position){
+function currentLocation(position) {
   let apiKey = "b81cb38c0b17e133191f4fac4a0b3833";
   let units = "metric";
   let endPointCoords = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=${units}`;
-  axios.get(endPointCoords).then(displayWeather)
+  axios.get(endPointCoords).then(displayWeather);
 }
 
-function getCurrentLocation(){
+function getCurrentLocation() {
   navigator.geolocation.getCurrentPosition(currentLocation);
 }
 
@@ -62,15 +99,17 @@ function searchCity(city) {
 
 function handleSubmit(event) {
   event.preventDefault();
-  searchCity(document.querySelector("#city-input").value);  
+  searchCity(document.querySelector("#city-input").value);
 }
 
 // Current Date & Time calls
 let now = new Date();
-document.querySelector("span.actual-date").innerHTML = formatDate(now)
+document.querySelector("span.actual-date").innerHTML = formatDate(now);
 document.querySelector("span.current-time").innerHTML = formatTime(now);
 
 // Weather in current location & searched city $ default city calls
-document.querySelector("#current-location").addEventListener("click", getCurrentLocation);
+document
+  .querySelector("#current-location")
+  .addEventListener("click", getCurrentLocation);
 document.querySelector("#search-city").addEventListener("submit", handleSubmit);
 searchCity("Prague");
